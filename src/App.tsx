e87b95570f960b41939c9ab476f9b233079e03e2
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2023-11-27 20:08:48 -0300

    feat: 🎸 Project recreation started

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { Dashboard } from "./components/Dashboard";
-import { Header } from "./components/Header";
-import { GlobalStyle } from "./styles/global";
 
 export function App() {
+
   return (
-    <>
-      <Header />
-      <Dashboard />
-      <GlobalStyle />
-    </>
+      <h1>
+          DT Money
+      </h1>
   )
 }
+
--- src/components/Dashboard/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.main`
-    max-width: 1240px;
-    margin: 0 auto;
-    padding: 32px 16px;
-`;

1ab61890e432a5486c25bc22efb6a8c2905108dc
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2024-01-14 20:17:24 -0300

    feat: 🎸 Axios settings

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
+import { api } from "../lib/axios.ts";
 
 export interface Transaction {
     id: number
@@ -27,16 +28,13 @@ export function TransactionsProvider ({ children }: TransactionsProviderProps) {
 
 
     async function fetchTransactions(query?: string) {
-        const url = new URL('http://localhost:3333/transactions')
+        const response = await api.get('transactions', {
+            params: {
+                q: query
+            }
+        })
 
-        if(query) {
-            url.searchParams.append('q', query)
-        }
-
-        const response = await fetch(url)
-        const data = await response.json()
-
-        setTransactions(data)
+        setTransactions(response.data)
     }
 
     useEffect(() => {
